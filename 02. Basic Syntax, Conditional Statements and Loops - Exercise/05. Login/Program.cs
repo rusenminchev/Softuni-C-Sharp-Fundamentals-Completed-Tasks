c46abc08bb61e779c03c1748989e7c5e0be290b3
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:10:48 +0200

    add

---
+++ 02. Basic Syntax, Conditional Statements and Loops - Exercise/05. Login/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            int failCount = 0;
+
+            string revUsername = new String(username.Reverse().ToArray());
+
+            while (password != revUsername)
+            {
+
+                failCount++;
+                if (failCount == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+                password = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"User {username} logged in.");
+        }
+    }
+}
