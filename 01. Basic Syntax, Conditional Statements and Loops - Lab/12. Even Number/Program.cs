b713e40c60c83dec219645c6fc5ce7c1fd4583e7
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:03:16 +0200

    add

---
+++ 01. Basic Syntax, Conditional Statements and Loops - Lab/12. Even Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _12._Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num % 2 == 0 && num != 0)
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(num)}");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Please write an even number.");
+                } 
+            }
+        }
+    }
+}
