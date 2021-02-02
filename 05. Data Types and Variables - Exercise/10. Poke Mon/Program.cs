ee24965c21bef124da4efa6b2a221c06d45641b8
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:18:32 +0200

    add

---
+++ 05. Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nPokePower = int.Parse(Console.ReadLine());
+            int mDistanceToTarget = int.Parse(Console.ReadLine());
+            byte yExhaustionFactor = byte.Parse(Console.ReadLine());
+
+            int currentN = nPokePower;
+
+            int targetCount = 0;
+
+            while (currentN >= mDistanceToTarget)
+            {
+                currentN -= mDistanceToTarget;
+                targetCount++;
+                if (currentN == nPokePower / 2)
+                {
+                    if (yExhaustionFactor > 0)
+                    {
+                        currentN /= yExhaustionFactor;
+                    }
+                }
+            }
+            Console.WriteLine(currentN);
+            Console.WriteLine(targetCount);
+        }
+    }
+}
