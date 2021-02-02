577d304d75cdcfb7cc589f537cbbca58fabd38b9
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:16:45 +0200

    add

---
+++ 04. Data Types and Variables - Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= num; i++)
+            {
+                int sum = 0;
+                int digits = i;
+                
+                while (digits > 0)
+                {
+                    sum += digits % 10;
+                    digits = digits / 10;
+                }
+                
+                if(sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
