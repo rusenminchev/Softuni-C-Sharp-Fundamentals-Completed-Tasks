8b421fba35dfd0894330501b58ec3688cac98e31
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:16:27 +0200

    add

---
+++ 04. Data Types and Variables - Lab/11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11._Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double volume = ((length * width) / 3) * height;
+
+            Console.Write("Length: ");
+            Console.Write("Width: ");
+            Console.Write("Height: ");
+            Console.WriteLine($"Pyramid Volume: {volume:f2}");
+        }
+    }
+}
