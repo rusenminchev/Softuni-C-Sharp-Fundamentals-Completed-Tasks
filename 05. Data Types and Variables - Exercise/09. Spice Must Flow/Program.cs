1050f58824ce535ec47500128f111dee44a264ce
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:18:28 +0200

    add

---
+++ 05. Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingSpice = int.Parse(Console.ReadLine());
+
+            int total = 0;
+            int daysCount = 0;
+
+            if (startingSpice >= 100)
+            {
+                while (startingSpice >= 100)
+                {
+                    daysCount++;
+                    total += startingSpice;
+                    startingSpice -= 10;
+                    total -= 26;
+                }
+
+                total -= 26;
+                Console.WriteLine(daysCount);
+                Console.WriteLine(total);
+            }
+            else
+            {
+                Console.WriteLine(daysCount);
+                Console.WriteLine(total);
+            }
+        }
+    }
+}
