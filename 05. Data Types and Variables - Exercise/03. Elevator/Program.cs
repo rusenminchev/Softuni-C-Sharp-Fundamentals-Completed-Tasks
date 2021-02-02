cd8a63875a92b8b6c528b547a5399770db1dc18e
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:18:02 +0200

    add

---
+++ 05. Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOFPeople = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+
+            double cources = Math.Ceiling(numOFPeople / (double)elevatorCapacity);
+
+            Console.WriteLine(cources);
+        }
+    }
+}
