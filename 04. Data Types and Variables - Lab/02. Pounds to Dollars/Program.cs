8a60d32374694ea80294c8ed96fd94b2ad08a39c
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:15:45 +0200

    add

---
+++ 04. Data Types and Variables - Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int units = int.Parse(Console.ReadLine());
+
+            double gbpToUsd = units * 1.31;
+
+            Console.WriteLine($"{gbpToUsd:f3}");
+        }
+    }
+}
