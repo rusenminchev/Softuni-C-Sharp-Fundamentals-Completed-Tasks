25b90ea0b1a14b9ee494bd7538c92fd935c77501
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:18:19 +0200

    add

---
+++ 05. Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte numberOfPouring = byte.Parse(Console.ReadLine());
+            
+            short sumOfLiters = 0;
+
+            for (int i = 1; i <= numberOfPouring; i++)
+            {
+                short litters = short.Parse(Console.ReadLine());
+
+                if (sumOfLiters + litters > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    sumOfLiters += litters;
+                } 
+            }
+            Console.WriteLine(sumOfLiters);
+        }
+    }
+}
