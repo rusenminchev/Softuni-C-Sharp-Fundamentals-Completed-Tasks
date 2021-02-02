2028142821afebfc556c87cb76ae75ccc8816a94
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:16:04 +0200

    add

---
+++ 04. Data Types and Variables - Lab/06. Reversed Chars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _06._Reversed_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+
+            Console.Write($"{c} {b} {a}");
+
+        }
+    }
+}
