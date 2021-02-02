0060f1c134dcdf41a23c75b0259385cd468174bc
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:02:15 +0200

    add

---
+++ 01. Basic Syntax, Conditional Statements and Loops - Lab/08. Divisible by 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _08._Divisible_by_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
