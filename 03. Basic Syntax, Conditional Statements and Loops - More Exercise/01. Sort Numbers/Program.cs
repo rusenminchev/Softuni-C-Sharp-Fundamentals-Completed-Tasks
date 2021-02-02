d0ec42fb7096f3082c68e0bcd453f5fc670813be
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:13:53 +0200

    add

---
+++ 03. Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace _01._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
