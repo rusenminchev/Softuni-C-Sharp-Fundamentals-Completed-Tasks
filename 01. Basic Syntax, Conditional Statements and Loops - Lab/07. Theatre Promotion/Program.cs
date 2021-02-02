e7b6270997453cbacbad4fa71ab3dd19c569d281
Rusen Minchev <rusenminchev@gmail.com>
2021-02-02 09:01:52 +0200

    add

---
+++ 01. Basic Syntax, Conditional Statements and Loops - Lab/07. Theatre Promotion/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace _07._Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfTheDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            int price = 0;
+
+            switch (typeOfTheDay)
+            {
+                case "Weekday":
+
+                    if (age >= 0 && age <= 18)
+                    {
+                        price = 12;
+                    }
+                    else if (age > 18 && age <= 64)
+                    {
+                        price = 18;
+                    }
+                    else if (age > 64 && age <= 122)
+                    {
+                        price = 12;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                        return;
+                    }
+
+                    break;
+
+                case "Weekend":
+
+                    if (age >= 0 && age <= 18)
+                    {
+                        price = 15;
+                    }
+                    else if (age > 18 && age <= 64)
+                    {
+                        price = 20;
+                    }
+                    else if (age > 64 && age <= 122)
+                    {
+                        price = 15;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                        return;
+                    }
+
+                    break;
+                    
+
+                case "Holiday":
+
+                    if (age >= 0 && age <= 18)
+                    {
+                        price = 5;
+                    }
+                    else if (age > 18 && age <= 64)
+                    {
+                        price = 12;
+                    }
+                    else if (age > 64 && age <= 122)
+                    {
+                        price = 10;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                        return;
+                    }
+
+                    break;
+
+            }
+
+            Console.WriteLine($"{price}$");
+        }
+    }
+}
