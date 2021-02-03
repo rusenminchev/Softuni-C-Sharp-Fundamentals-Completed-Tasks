7384c31ca00b0f67ccf481ae2fb4d339f8ed7da1
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:50:12 +0200

    add

---
+++ 11. Lists - Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> arrays = Console.ReadLine().Split('|').Where(s => !string.IsNullOrWhiteSpace(s)).Reverse().ToList();
+
+
+
+            for (int i = arrays.Count - 1; i >= 0; i--)
+            {
+                arrays[i] = arrays[i].Trim();
+                
+                    arrays[i] = arrays[i].Replace("  ", " ");
+            
+            }
+
+            Console.WriteLine(string.Join(' ', arrays));
+
+        }
+    }
+}
