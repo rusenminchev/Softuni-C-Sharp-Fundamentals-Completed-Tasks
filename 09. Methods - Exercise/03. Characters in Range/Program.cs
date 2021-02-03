a8bd5048ada9c23a0fb66dc9f8fa50bfdd5f4e27
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:39:19 +0200

    add

---
+++ 09. Methods - Exercise/03. Characters in Range/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch1 = char.Parse(Console.ReadLine());
+            char ch2 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine(CharactersBetweenTwoChars(ch1, ch2));
+        }
+
+        static string CharactersBetweenTwoChars(char ch1, char ch2)
+        {
+
+            string output = "";
+            if (ch1 < ch2)
+            {
+                for (int i = ch1 + 1; i < ch2; i++)
+                {
+                    output += (char)i + " ";
+                }
+            }
+            else
+            {
+                for (int i = ch2 + 1; i < ch1; i++)
+                {
+                    output += (char)i + " ";
+                }
+
+            }
+
+            return output;
+        }
+
+    }
+}
