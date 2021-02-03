9dc57dac9c3996b0451510c9c7ced5a74e6a7d54
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:52:37 +0200

    add

---
+++ 14. Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split().ToList();
+
+            Dictionary<string, int> count = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string wordsToLower = word.ToLower();
+
+                if (count.ContainsKey(wordsToLower))
+                {
+                    count[wordsToLower]++;
+                }
+                else
+                {
+                    count.Add(wordsToLower, 1);
+                }
+            }
+
+            foreach (var word in count)
+            {
+                if (word.Value % 2 == 1)
+                {
+                    Console.Write(word.Key + " ");
+                }
+            }
+        }
+    }
+}
