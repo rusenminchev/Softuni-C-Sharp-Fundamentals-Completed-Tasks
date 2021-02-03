ba64d50a5816a79f83f27f39697f3caab5eae506
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:52:52 +0200

    add

---
+++ 14. Associative Arrays - Lab/05. Word Filter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split().ToList();
+
+            List<string> evenWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                if (word.Length % 2 == 0)
+                {
+                    evenWords.Add(word);
+                }
+            }
+
+            Console.WriteLine(String.Join("\n", evenWords ));
+
+        }
+    }
+}
