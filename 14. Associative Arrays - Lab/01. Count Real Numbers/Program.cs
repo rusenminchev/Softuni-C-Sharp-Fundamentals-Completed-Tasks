09ff112d9d0261a974bef07c34b90b1f5b27e5a3
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:52:31 +0200

    add

---
+++ 14. Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> count = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (count.ContainsKey(number))
+                {
+                    count[number]++;
+                }
+                else
+                {
+                    count.Add(number, 1);
+                }
+            }
+
+            foreach (var number in count)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
