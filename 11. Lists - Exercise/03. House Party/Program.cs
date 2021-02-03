0b34891b531e242db07f7bc1c4dc709ae1e1ab3c
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:49:48 +0200

    add

---
+++ 11. Lists - Exercise/03. House Party/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._House_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> guestList = new List<string>();
+
+            List<string> command = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                command = Console.ReadLine().Split().ToList();
+                string name = command[0];
+
+                if (command[2] == "going!")
+                {
+                    if (guestList.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        guestList.Add(name);
+                    }
+                }
+                else if (command[2] == "not")
+                {
+                    if (guestList.Contains(name))
+                    {
+                        guestList.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+
+
+            }
+
+            for (int i = 0; i < guestList.Count; i++)
+            {
+                Console.WriteLine(guestList[i]);
+            }
+
+        }
+    }
+}
---
+++ 11. Lists - Exercise/04. List Operations/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace _04._List_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string commands = string.Empty;
+
+            while ((commands = Console.ReadLine()) != "End")
+            {
+                List<string> elements = commands.Split().ToList();
+                string command = elements[0];
+
+                if (command == "Add")
+                {
+                    int number = int.Parse(elements[1]);
+                    input.Add(number);
+
+                }
+                else if (command == "Insert")
+                {
+                    int number = int.Parse(elements[1]);
+                    int index = int.Parse(elements[2]);
+                    
+                    if (index >= 0 && index <= input.Count - 1)
+                    { 
+                        input.Insert(index, number);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    
+                }
+                else if (command == "Remove")
+                {
+                    int index = int.Parse(elements[1]);
+                    if (index >= 0 && index <= input.Count - 1)
+                    {
+                        input.RemoveAt(index);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                }
+                else if (command == "Shift")
+                {
+                    if (elements[1] == "left")
+                    {
+                        int count = int.Parse(elements[2]);
+
+                        for (int i = 0; i < count; i++)
+                        {
+                            int firstNum = input[0];
+                            input.Remove(input[0]);
+                            input.Add(firstNum);
+                            
+                        }
+                    }
+                    else if ((elements[1] == "right"))
+                    {
+                        int lastNum = input[input.Count - 1];
+                        input.Remove(input[input.Count - 1]);
+                        input.Insert(0, lastNum);
+                    }
+
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", input));
+          
+        }
+    }
+}
