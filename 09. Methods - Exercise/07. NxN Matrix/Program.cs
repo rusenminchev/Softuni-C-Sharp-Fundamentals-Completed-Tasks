de31ea96681090c8f43aea21acda3a1e7c61b4dd
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:40:03 +0200

    add

---
+++ 09. Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            CreateNxNMatrix(n);
+        }
+
+        static void CreateNxNMatrix(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+            }
+        
+        }
+
+
+    }
+}
