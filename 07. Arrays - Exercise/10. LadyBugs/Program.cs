9c9da66a8c2bc6854e6b23acedfc5a99d8ce50f6
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:34:08 +0200

    add

---
+++ 07. Arrays - Exercise/10. LadyBugs/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+
+            int[] initiialIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] field = new int[fieldSize];
+
+            for (int i = 0; i < initiialIndexes.Length; i++)
+            {
+                int currentIndex = initiialIndexes[i];
+
+                if (currentIndex >= 0 && currentIndex < field.Length)
+                {
+                    field[currentIndex] = 1;
+                }  
+            }
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] element = command.Split();
+                int ladybugIndex = int.Parse(element[0]);
+                string direction = element[1];
+                int flightLength = int.Parse(element[2]);
+
+
+                if (ladybugIndex < 0 || ladybugIndex > field.Length - 1 || field[ladybugIndex] == 0)
+                {
+                    continue;
+                }
+                
+                field[ladybugIndex] = 0;
+
+                if (direction == "right")
+                {
+                    int landIndex = ladybugIndex + flightLength;
+
+                    if (landIndex > field.Length - 1)
+                    {
+                        continue;
+                    }
+
+                    if (field[landIndex] == 1)
+                    {
+                        while (field[landIndex] == 1)
+                        {
+                            landIndex += flightLength;
+                            if (landIndex > field.Length - 1)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (landIndex >= 0 && landIndex <= field.Length - 1)
+                    {
+                        field[landIndex] = 1;
+                    }
+                }
+                else if (direction == "left")
+                {
+                    int landIndex = ladybugIndex - flightLength;
+
+                    if (landIndex < 0)
+                    {
+                        continue;
+                    }
+
+                    if (field[landIndex] == 1)
+                    {
+                        while (field[landIndex] == 1)
+                        {
+                            landIndex -= flightLength;
+                            if (landIndex < 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (landIndex >= 0 && landIndex <= field.Length - 1)
+                    {
+                        field[landIndex] = 1;
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", field));
+        }
+    }
+}
