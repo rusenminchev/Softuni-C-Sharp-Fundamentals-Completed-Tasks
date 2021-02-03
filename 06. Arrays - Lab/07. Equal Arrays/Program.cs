6d6839405ead51fa6e998afd97cdc793f8886c4a
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:29:38 +0200

    add

---
+++ 06. Arrays - Lab/07. Equal Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace _07._Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int diffIndex = 0;
+            int sum = 0;
+            bool isDiff = false;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] == arr2[i])
+                {
+                    sum += arr1[i];
+                }
+                else if (arr1[i] != arr2[i])
+                {
+                    isDiff = true;
+                    diffIndex = i;
+                    break;
+                }
+            }
+
+            if (isDiff == true)
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {diffIndex} index");
+                
+            }
+            else
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+
+        }
+    }
+}
