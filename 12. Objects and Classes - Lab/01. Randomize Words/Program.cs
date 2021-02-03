17c247f62965f83592811b9794e4cd3cf792bf51
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:50:50 +0200

    add

---
+++ 12. Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Randomize_Words
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Randomizer words = new Randomizer();
+            words.words = Console.ReadLine().Split();
+            words.Randomiser();
+            words.PrintWords();
+        }
+
+        public class Randomizer
+        {
+            public string[] words;
+            public void Randomiser()
+            {
+                Random rand = new Random();
+                for (int i = 0; i < words.Length; i++)
+                {
+                    int randomPisition = rand.Next(0, words.Length);
+                    string temp = words[i];
+                    words[i] = words[randomPisition];
+                    words[randomPisition] = temp;
+                }
+            }
+
+            public void PrintWords()
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, words));
+            }
+        }
+    }
+}
