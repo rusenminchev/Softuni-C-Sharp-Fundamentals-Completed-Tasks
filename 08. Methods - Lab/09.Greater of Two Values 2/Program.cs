e26cf5e8d1b90df41984d032fb0340ff37718ebc
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:37:26 +0200

    add

---
+++ 08. Methods - Lab/09.Greater of Two Values 2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _09.Greater_of_Two_Values_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if (type == "int")
+            {
+
+                int v1 = int.Parse(Console.ReadLine());
+                int v2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(GetGreater(v1, v2));
+
+            }
+
+            if (type == "char")
+            {
+
+                char v1 = char.Parse(Console.ReadLine());
+                char v2 = char.Parse(Console.ReadLine());
+                Console.WriteLine(GetGreater(v1, v2));
+
+            }
+
+            if (type == "string")
+            {
+                string v1 = Console.ReadLine();
+                string v2 = Console.ReadLine();
+                Console.WriteLine(GetGreater(v1, v2));
+
+            }
+        }
+
+        static int GetGreater(int v1, int v2)
+        {
+            int compare = v1.CompareTo(v2);
+
+            if (compare > 0)
+            {
+                return v1;
+            }
+            else
+            {
+                return v2;
+            }
+        }
+
+        static char GetGreater(char v1, char v2)
+        {
+            int compare = v1.CompareTo(v2);
+
+            if (compare > 0)
+            {
+                return v1;
+            }
+            else
+            {
+                return v2;
+            }
+        }
+
+        static string GetGreater(string v1, string v2)
+        {
+            int compare = v1.CompareTo(v2);
+
+            if (compare > 0)
+            {
+                return v1;
+            }
+            else
+            {
+                return v2;
+            }
+        }
+    }
+}
