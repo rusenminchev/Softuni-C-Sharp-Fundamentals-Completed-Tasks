7de65e98f917197136493b4b04fa6faa25a461c1
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:59:27 +0200

    C# Fundamentals Final Exam

---
+++ C Sharp - Programming Fundamentals - Final Exam/01. The Imitation Game/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Problem_1._The_Imitation_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+
+            string command = Console.ReadLine();
+
+            while (command != "Decode")
+            {
+                string[] tokens = command.Split('|', StringSplitOptions.RemoveEmptyEntries);
+                string action = tokens[0];
+
+                if (action == "Move")
+                {
+                    int numberOfLetters = int.Parse(tokens[1]);
+
+                    string substring = encryptedMessage.Substring(0, numberOfLetters);
+                    encryptedMessage = encryptedMessage.Remove(0, numberOfLetters);
+                    encryptedMessage += substring;
+                }
+                else if (action == "Insert")
+                {
+                    int index = int.Parse(tokens[1]);
+                    string valueToInsert = tokens[2];
+
+                    encryptedMessage = encryptedMessage.Insert(index, valueToInsert);
+
+                }
+                else if (action == "ChangeAll")
+                {
+                    string substring = tokens[1];
+                    string replacement = tokens[2];
+
+                    encryptedMessage = encryptedMessage.Replace(substring, replacement);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {encryptedMessage}");
+        }
+    }
+}
---
+++ C Sharp - Programming Fundamentals - Final Exam/03. The Pianist/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._The_Pianist
+{
+
+    class ComposerKey
+    {
+        public string Composer { get; set; }
+        public string Key { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, ComposerKey> pianistInfo = new Dictionary<string, ComposerKey>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split('|');
+                string piece = input[0];
+                string composer = input[1];
+                string key = input[2];
+
+                ComposerKey composerKey = new ComposerKey() { Composer = composer, Key = key };
+
+                pianistInfo.Add(piece, composerKey);
+            }
+
+            string commands = Console.ReadLine();
+
+            while (commands != "Stop")
+            {
+                string[] tokens = commands.Split('|');
+                string action = tokens[0];
+
+
+                if (action == "Add")
+                {
+                    string piece = tokens[1];
+                    string composer = tokens[2];
+                    string key = tokens[3];
+
+                    if (pianistInfo.ContainsKey(piece))
+                    {
+                        Console.WriteLine($"{piece} is already in the collection!");
+                    }
+                    else
+                    {
+                        pianistInfo.Add(piece, new ComposerKey() { Composer = composer, Key = key });
+                        Console.WriteLine($"{piece} by {composer} in {key} added to the collection!");
+                    }
+                }
+                else if (action == "Remove")
+                {
+                    string piece = tokens[1];
+
+                    if (pianistInfo.ContainsKey(piece))
+                    {
+                        pianistInfo.Remove(piece);
+                        Console.WriteLine($"Successfully removed {piece}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+
+                }
+                else if (action == "ChangeKey")
+                {
+
+                    string piece = tokens[1];
+                    string newKey = tokens[2];
+
+                    if (pianistInfo.ContainsKey(piece))
+                    {
+                        pianistInfo[piece].Key = newKey;
+                        Console.WriteLine($"Changed the key of {piece} to {newKey}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+                }
+
+                commands = Console.ReadLine();
+            }
+
+            pianistInfo = pianistInfo.OrderBy(x => x.Key).ThenBy(x => x.Value.Composer).ToDictionary(x => x.Key, x => x.Value);
+
+
+            foreach (var item in pianistInfo)
+            {
+                Console.WriteLine($"{item.Key} -> Composer: {item.Value.Composer}, Key: {item.Value.Key}");
+            }
+
+        }
+    }
+}
