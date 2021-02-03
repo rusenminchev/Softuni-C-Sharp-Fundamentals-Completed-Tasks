8f02b296c7a863cdc8ebb6f7d28ba243d6ec42c4
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:33:28 +0200

    add

---
+++ 07. Arrays - Exercise/04. Array Rotation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+        
+            for (int j = 0; j < n; j++)
+            {
+
+                int first = array[0];
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {         
+                    array[i] = array[i + 1];
+                }
+                array[array.Length - 1] = first;
+            }
+            Console.WriteLine(String.Join(" ", array));
+        }
+    }
+}
