41d99d7ce89e5a2efc6dc280db110efce8a3d591
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:30:04 +0200

    add

---
+++ 06. Arrays - Lab/09. Condense Array to Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+          
+            while (nums.Length != 1)
+            {
+
+                int[] condences = new int[nums.Length - 1];
+
+                for (int i = 0; i < condences.Length; i++)
+                {
+                    condences[i] = nums[i] + nums[i + 1];
+                }
+
+                nums = condences;
+                
+            }
+
+            Console.WriteLine(nums[0]);
+
+        }
+    }
+}
