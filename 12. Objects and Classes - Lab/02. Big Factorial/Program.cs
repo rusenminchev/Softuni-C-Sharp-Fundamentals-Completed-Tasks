b6470ca34c3572f74b27f1b0401b4f2341fe07ca
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:50:55 +0200

    add

---
+++ 12. Objects and Classes - Lab/02. Big Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Dynamic;
+using System.Numerics;
+
+namespace _3._Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigFactorial result = new BigFactorial();
+            result.n = int.Parse(Console.ReadLine());
+            result.GetFactorilNumber();
+        }
+
+        public class BigFactorial
+        {
+            public BigInteger factorial;
+            public int n;
+
+            public void GetFactorilNumber()
+            {
+                factorial = n;
+                for (int i = n - 1; i > 0; i--)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine(factorial);
+            }
+        }
+    }
+}
