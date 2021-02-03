6f6259b32132e44773a2cf4ca00f62d240ddf6b6
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:39:58 +0200

    add

---
+++ 09. Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(GetTheMiddleCharacters(input));
+        }
+
+        static string GetTheMiddleCharacters(string input)
+        {
+            if (input.Length % 2 == 1)
+            {
+                int middleCharIndex = input.Length / 2;
+
+                return input[middleCharIndex].ToString();
+            }
+            else if (input.Length % 2 == 0)
+            {
+                string evenOutput = "";
+                int middleCharIndex = input.Length / 2;
+
+
+                return evenOutput += input[middleCharIndex - 1].ToString() + input[middleCharIndex].ToString();
+            }
+            return "";
+        }
+
+    }
+}
