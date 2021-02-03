f4780b53b897af4347b49e1a7f0d47d8346f1952
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:29:58 +0200

    add

---
+++ 06. Arrays - Lab/08. again/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._again
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            while (numbers.Length != 1)
+            {
+
+                int[] condences = new int[numbers.Length - 1];
+
+                for (int i = 0; i < condences.Length; i++)
+                {
+                    condences[i] = numbers[i] + numbers[i + 1];
+                }
+
+                numbers = condences;
+
+            }
+
+            Console.WriteLine(numbers[0]);
+
+        }
+    }
+}
