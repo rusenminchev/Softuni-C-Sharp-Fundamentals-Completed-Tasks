50c4b261f40607b4b8faa92d4c176f73b1d65ebc
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:33:54 +0200

    add

---
+++ 07. Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int k = i + 1; k < array.Length; k++)
+                {
+                    if (array[i] + array[k] == n)
+                    {
+                        Console.WriteLine(array[i]+ " " + array[k]);
+                    }
+                }
+            }
+        }
+    }
+}
