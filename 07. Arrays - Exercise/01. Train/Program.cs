c9ae346605d4be58ccd9ef37a424ff2fa0b46031
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:33:01 +0200

    add

---
+++ 07. Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] people = new int[n];
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                people[i] = int.Parse(Console.ReadLine());
+                sum += people[i];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(people[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
