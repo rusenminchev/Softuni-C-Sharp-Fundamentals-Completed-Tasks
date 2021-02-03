e7462672b4b9d8007255f872672cd935e2ccbf4c
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:54:33 +0200

    add

---
+++ 16. Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            string names = Console.ReadLine();
+
+            MatchCollection machedNames = Regex.Matches(names, regex);
+
+            foreach (Match name in machedNames)
+            {
+
+                Console.Write(name.Value + " ");
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
