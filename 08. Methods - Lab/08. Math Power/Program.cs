a6903fb96db74f2ecbe9f9ec7922bfeb12e3d0f1
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:36:46 +0200

    add

---
+++ 08. Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            double result = GivePowerToTheNumber(number, power);
+            Console.WriteLine(result);
+        }
+
+        static double GivePowerToTheNumber(double num, double pow)
+        {
+            double originalNum = num;
+
+            for (int i = 1; i < pow; i++)
+            {
+                num = num * originalNum;       
+            }
+            return num;
+        }
+    }
+}
