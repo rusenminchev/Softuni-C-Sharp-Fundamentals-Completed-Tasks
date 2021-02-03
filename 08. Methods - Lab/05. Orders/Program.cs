da39c3ea388c3f06c502b380d4d2ab92973bd913
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:36:23 +0200

    add

---
+++ 08. Methods - Lab/05. Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            switch (product)
+            {
+                case "coffee":
+                    CalculatesTheTotalPrice(quantity, 1.50);
+                    break;
+
+                case "water":
+                    CalculatesTheTotalPrice(quantity, 1.00);
+                    break;
+
+                case "coke":
+                    CalculatesTheTotalPrice(quantity, 1.40);
+                    break;
+
+                case "snacks":
+                    CalculatesTheTotalPrice(quantity, 2.00);
+                    break;
+            }
+        }
+
+        static void CalculatesTheTotalPrice(int quantity, double price)
+        {
+            Console.WriteLine($"{quantity* price:f2}");
+        }
+    }
+}
