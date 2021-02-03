fb2098578cb7e422cf807d02ed2cb1110517d195
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:53:33 +0200

    add

---
+++ 15. Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> output = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "stop")
+                {
+                    break;
+                }
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (output.ContainsKey(command))
+                {
+                    command += quantity;
+                }
+                else
+                {
+                    output.Add(command, quantity);
+                }
+            }
+
+            foreach (var resourse in output)
+            {
+                Console.WriteLine($"{resourse.Key} -> {resourse.Value}");
+            }
+        }
+    }
+}
