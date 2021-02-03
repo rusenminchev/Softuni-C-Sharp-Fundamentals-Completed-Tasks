a71314026834135644415d55de9a70defdbb86cf
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:36:40 +0200

    add

---
+++ 08. Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToRepeat = Console.ReadLine();
+            int repeatCount = int.Parse(Console.ReadLine());
+
+            string output = RepeatStringNTimes(stringToRepeat, repeatCount);
+
+            Console.WriteLine(output);
+
+        }
+
+        static string RepeatStringNTimes(string toRepeat, int counter)
+        {
+            string finalString = "";
+            for (int i = 0; i < counter; i++)
+            {
+                finalString += toRepeat;
+            }
+            return finalString;
+        }
+    }
+}
