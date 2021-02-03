513243c012f52e5d0050a2406de1106961495f54
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:49:11 +0200

    add

---
+++ 10. Lists - Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> list2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> mergedList = new List<int>();
+
+            int bigger = Math.Max(list1.Count, list2.Count);
+
+            for (int i = 0; i < bigger; i++)
+            {
+                while (i <= list1.Count && i <= list2.Count)
+                {
+                    mergedList.Add(list1[i]);
+                    mergedList.Add(list2[i]);
+                }
+                
+            }
+            Console.WriteLine(String.Join(" ", list2));
+        }
+    }
+}
