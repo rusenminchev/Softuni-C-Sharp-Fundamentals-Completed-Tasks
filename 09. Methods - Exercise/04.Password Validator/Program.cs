638a96fdbd7e79f857b3a5d22c2c0c8bcd1f47e0
Rusen Minchev <rusenminchev@gmail.com>
2021-02-03 20:39:42 +0200

    add

---
+++ 09. Methods - Exercise/04.Password Validator/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _04.Password_Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            CountTheNumberOfCharacters(password);
+            
+        }
+
+        static void CountTheNumberOfCharacters(string password)
+        {
+            bool invalid = false;
+
+            if (password.Length < 6 || password.Length > 10)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+                invalid = true;
+            }
+            if (IsItConsistsOnlyLettersAndDigits(password) > 0)
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+                invalid = true;
+            }
+            if (IsItContainsAtLeast2Digits(password) < 2)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+                invalid = true;
+            }
+            if (invalid == false)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+
+        static int IsItConsistsOnlyLettersAndDigits(string password)
+        {
+            bool valid = true;
+            int otherSymbolsCount = 0;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                valid = char.IsLetterOrDigit(password[i]);
+                if (valid == false)
+                {
+                    otherSymbolsCount++;
+                    break;
+                }
+            }
+
+            return otherSymbolsCount;
+        }
+
+        static int IsItContainsAtLeast2Digits(string password)
+        {
+            int digitsCounter = 0;
+            bool IsDigit = false;
+
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                IsDigit = char.IsDigit(password[i]);
+                if (IsDigit == true)
+                {
+                    digitsCounter++;
+                }
+                IsDigit = false;
+            }
+
+            return digitsCounter;
+        }
+    }
+}
